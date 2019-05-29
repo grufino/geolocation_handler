e0e05a9f4b4d54bc0aefa69cd2efd800d7124326
Gabriel Rufino <gabriel@johan-sports.com>
2019-05-29 20:34:55 +0200

    project creation

---
+++ lib/geolocation_handler.ex
@@ -0,0 +1,18 @@
+defmodule GeolocationHandler do
+  @moduledoc """
+  Documentation for GeolocationHandler.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> GeolocationHandler.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/geolocation_handler_test.exs
@@ -0,0 +1,8 @@
+defmodule GeolocationHandlerTest do
+  use ExUnit.Case
+  doctest GeolocationHandler
+
+  test "greets the world" do
+    assert GeolocationHandler.hello() == :world
+  end
+end
